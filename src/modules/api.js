95bb757700241bb9ab3f20e78ee90a25cd60a7e5
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-04 14:25:57 -0300

    add get

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,2 +1,45 @@
-const query = "";
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('search', () => {
+  const query = searchInput.value;
+  // here I have to return in a specific html element the list of movie letters
+});
+
+const getArticles = async (query) => {
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    const data = await response.json();
+    const articles = data.map((item) => ({
+      id: item.show.id,
+      name: item.show.name,
+    }));
+
+    return articles;
+  } catch (error) {
+    console.error('Error:', error);
+    return [];
+  }
+};
+
+const getArticleDetails = async (articleId) => {
+  try {
+    const response = await fetch(`https://api.tvmaze.com/shows/${articleId}`);
+    const data = await response.json();
+
+    const articleDetails = {
+      id: data.id,
+      name: data.name,
+      summary: data.summary,
+
+    };
+
+    return articleDetails;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+};
+
 const api = `https://api.tvmaze.com/search/shows?q=${query}`;
+
+
