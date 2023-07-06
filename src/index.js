902a9aafdd0a2932592e59e3dbac155b82220317
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-06 01:47:25 -0300

    finalize details to merge

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import "./style.css";
 import "./modules/apiMovies.js";
 import "./modules/apiLikes.js";
 import { addCards } from "./modules/cards.js";
-import { addCommentForm } from "./modules/addCommentForm.js";
+// import { addCommentForm } from "./modules/addCommentForm.js";
 
 window.addEventListener("load", () => {
   addCards();
-  addCommentForm();
+  // addCommentForm();
 });
 
 console.log("Hello webpack!");
