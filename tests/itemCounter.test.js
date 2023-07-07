8107d554cf881e0b56d7e0cbc07ad12390565c73
haben mehar <habenmehari71@gmail.com>
2023-07-07 12:43:06 +0300

    item counter refactored and test added

---
+++ tests/itemCounter.test.js
@@ -0,0 +1,10 @@
+import {countNumberOfShows} from '../src/modules/utils';
+
+describe('item counter test case for items in home page.', () => { 
+    test('should update dom element with correct number of items.', () => {
+        const domElement = document.createElement('p');
+        const shows = [{ id: 1, name: 'test 1' }, { id: 2, name: 'test 2' }];
+        countNumberOfShows(domElement, shows);
+        expect(domElement.innerText).toBe(3);
+     });
+ })
